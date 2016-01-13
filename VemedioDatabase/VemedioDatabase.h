06e307f2c2fae58489215d49880831e2fcba7678
Martin Hering <hering@vemedio.com>
2016-01-13 10:26:19 +0100

    Initial Commit

---
+++ VemedioDatabase/VemedioDatabase.h
@@ -0,0 +1,19 @@
+//
+//  VemedioDatabase.h
+//  VemedioDatabase
+//
+//  Created by Martin Hering on 13.01.16.
+//  Copyright © 2016 Vemedio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VemedioDatabase.
+FOUNDATION_EXPORT double VemedioDatabaseVersionNumber;
+
+//! Project version string for VemedioDatabase.
+FOUNDATION_EXPORT const unsigned char VemedioDatabaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VemedioDatabase/PublicHeader.h>
+
+
